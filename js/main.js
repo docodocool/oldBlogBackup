9c7f1e21bdabb3489f4e7813cc65c5c13f99284d
coolucifer <coolucifer@gmail.com>
2017-10-28 10:10:46 +0800

    commit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 //smartFloat()来自于:http://www.cnblogs.com/bobodeboke/p/4565382.html
-$.fn.smartFloat = function (para) {
+$.fn.smartFloat = function (dis,cla) {
     var position = function (element) {
         var top = element.offset().top; //当前元素element距浏览器上边距
         $(window).scroll(function () { //侦听滚动
             var scrolls = $(this).scrollTop();
-            if (scrolls - top > para) { //如果滚动到页面超出了当前元素element的相对页面顶部的高度 
-                element.addClass("navbar-fixed-top");
+            if (scrolls - top > dis) { //如果滚动到页面超出了当前元素element的相对页面顶部的高度 
+                element.addClass(cla);
             } else {
-                element.removeClass("navbar-fixed-top");
+                element.removeClass(cla);
             };
         });
     };
@@ -16,9 +16,56 @@ $.fn.smartFloat = function (para) {
     });
 };
 
+// $(".side-bar").affix({
+//     offset:{
+//         top:$(".side-bar").offset().top-70,
+//         bottom:($("footer").outerHeight(true))+40,
+//     }
+// });
+
 $(function () {
     $("#bgimg").height($(window).height());
-    $(".headerbox").height($(window).height() - 75);
-    $(".side-bar").height($(window).height() - 150);
-    $(".navbar").smartFloat(0);
+    $(".headerbox").height($(window).height() - 65);
+    // $(".side-bar").height($(window).height() - 150);
+    $(".navbar").smartFloat(0,"navbar-fixed-top");
+});
+
+
+var mainpage=new Vue({
+    el:"#mainpage",
+    methods:{
+        onClick:function(){
+            var loaded=false;
+            if(!loaded){
+                $("#show").load('./mainpage.html');
+                loaded=true;
+            }
+        }
+    }
+});
+
+var shwoblog=new Vue({
+    el:"#showblog",
+    methods:{
+        onClick:function(){
+            var loaded=false;
+            if(!loaded){
+                $("#show").load('./test.html');
+                loaded=true;
+            }
+        }
+    }
+});
+
+var shwopractices=new Vue({
+    el:"#showpractices",
+    methods:{
+        onClick:function(){
+            var loaded=false;
+            if(!loaded){
+                $("#show").load('./practices/index.html');
+                loaded=true;
+            }
+        }
+    }
 });
