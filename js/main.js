96dcb9a8dbbcbc49dbf375eede51bb07b6e6abc8
coolucifer <coolucifer@gmail.com>
2017-12-08 15:40:20 +0800

    commit

--- js/main.js
+++ js/main.js
@@ -16,18 +16,10 @@ $.fn.smartFloat = function (dis, cla) {
     });
 };
 
-var if_less = function () {
-    if ($(window).height - 85 < 537) {
-        return 537;
-    } else {
-        return $(window).height - 85;
-    }
-}
-
 $(function () {
     $("#bgimg").height($(window).height());
     $(".headerbox").height($(window).height() - 65);
-    $(".contain").css("min-height", if_less);
+    $(".contain").height(($(window).height() - 85 < 537) ? 537 : ($(window).height() - 85));
     $(".fix-nav").smartFloat(0, "navbar-fixed-top");
 
 
