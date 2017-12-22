5d7a9292c712302fc4ec3c47f4c19c2e1e8e7ab1
coolucifer <coolucifer@gmail.com>
2017-12-22 20:11:14 +0800

    commit

--- js/main.js
+++ js/main.js
@@ -142,7 +142,7 @@ var routes = [{
           <a href="https://coolucifer.github.io/simpleblog/index.html" class="list-group-item">简单博客</a>
           <a href="https://coolucifer.github.io/jQuery-Form-Validation/index.html" class="list-group-item">JQuery实战:表单验证</a>
           <a href="https://coolucifer.github.io/xiuzhentasks/变色九宫格/index.html" class="list-group-item">变色九宫格</a>
-          <a href="https://coolucifer.github.io/vue-list/index.html" class="list-group-item">Vue-list</a>
+          <a href="./projects/vue-list.html" class="list-group-item">Vue-list</a>
           <a href="./projects/2048.html" class="list-group-item">2048</a>
         </div>
       </div>
